221f7b0fc186a81adf44fa32d0036933658943d1
YeeunKong <yeeungong1014@gmail.com>
2022-03-03 23:16:01 +0900

    3.3_update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector(".hello h1");
+//className-> .hello
+//id-> #hello
+//className이나 id 뒤에 tagName(<h1>)
 
-let guri = 3;
-
-console.log(age, typeof age);
-
+title.innerText = "hello!!!!!";
+//지정된 element의 innerText를 바꿀 수 있음!!
